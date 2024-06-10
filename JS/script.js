e11c34fc560c5b1781dadd5a85003ab6cef9ae77
Nancy-Shrown <nancyshrown@gmail.com>
2024-06-10 12:26:16 +0530

    completed

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+console.log('Hello World')
+
+//variables
+
+//1  var
+var FirstName="GOJO"
+console.log(FirstName)
+
+//2  let
+let LastName="SATORU"
+console.log(LastName)
+
+//3  Const
+const FullName="SATORU GOJO"
+console.log(FullName)
+
+
